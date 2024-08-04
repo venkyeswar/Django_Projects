994bea9002fdaae9621267b95ae3879a89c672e0
venky <venkyeswar2037@gmail.com>
2024-08-05 02:10:58 +0530

    modified

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from os import getenv
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT =BASE_DIR/'staticfiles'
+STATIC_ROOT =os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = "static/"
 
 MEDIA_URLS = "/files/"
-MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
